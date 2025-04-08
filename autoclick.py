107be352e1fe79ee3a2ad1b09ad76cccf7a76583
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-08 17:30:18 -0300

    Entradas para email, alerta, senha e confirmação

--- autoclick.py
+++ autoclick.py
@@ -1,19 +1,10 @@
 import pyautogui
-import pyperclip
-from time import sleep
 
-def escrever(frase):
-    pyperclip.copy(frase)
-    pyautogui.hotkey('ctrl', 'v')
+pyautogui.alert(text='Estamos entrando em nossa Automação', title='Aviso')
 
-pyautogui.click(325,1058, duration=2)
-pyautogui.click(363,19, duration=2)
-pyautogui.click(215,59, duration=2)
-escrever('data')
-pyautogui.click(202,143, duration=2)
-pyautogui.click(770,429, duration=2)
-escrever('viniciuskeglesads@gmail.com')
-sleep(2)
-pyautogui.press('tab')
-escrever('Pernamacabro@2')
-pyautogui.click(706,646, duration=2)
+email = pyautogui.prompt(text='Digite seu e-mail', title='Informações Obrigatórias')
+print(f'você digitou {email}')
+
+pyautogui.confirm(text=f'Continuar rodando nossa automação com o email {email}?', title='Confirmação', buttons=['Sim', 'Não', 'Cancelar'])
+
+pyautogui.password(text='Digite sua senha:', title='Senha de Login', mask='*')

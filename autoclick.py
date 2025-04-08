fec8688e085d90a55b9997fc4cbe12fb5a1f9daa
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-08 16:32:20 -0300

    Login e Senha automatico

--- autoclick.py
+++ autoclick.py
@@ -1,13 +1,19 @@
 import pyautogui
 import pyperclip
+from time import sleep
 
-def escrever_frase(frase):
+def escrever(frase):
     pyperclip.copy(frase)
     pyautogui.hotkey('ctrl', 'v')
 
-pyautogui.click(406,182, duration=2)
-escrever_frase('Mãe')
-pyautogui.click(422,330, duration=2)
-pyautogui.click(900,993, duration=2)
-escrever_frase('Te amo mãe <3')
-pyautogui.click(1648,993, duration=2)
+pyautogui.click(325,1058, duration=2)
+pyautogui.click(363,19, duration=2)
+pyautogui.click(215,59, duration=2)
+escrever('data')
+pyautogui.click(202,143, duration=2)
+pyautogui.click(770,429, duration=2)
+escrever('viniciuskeglesads@gmail.com')
+sleep(2)
+pyautogui.press('tab')
+escrever('Pernamacabro@2')
+pyautogui.click(706,646, duration=2)

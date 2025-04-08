469e0a5df4c2dcc2846347d9e49df4dba8703531
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-08 11:33:54 -0300

    Enviando msg 'Te amo Mae' automatico

--- autoclick.py
+++ autoclick.py
@@ -5,7 +5,9 @@ def escrever_frase(frase):
     pyperclip.copy(frase)
     pyautogui.hotkey('ctrl', 'v')
 
-pyautogui.click(403,572, duration=3)
-pyautogui.click(880,994, duration=3)
-pyautogui.typewrite('Oi amor, estou te enviando essa mensagem via codigo! Te amo <3')
-pyautogui.click(1647,992, duration=3)
+pyautogui.click(406,182, duration=2)
+escrever_frase('Mãe')
+pyautogui.click(422,330, duration=2)
+pyautogui.click(900,993, duration=2)
+escrever_frase('Te amo mãe <3')
+pyautogui.click(1648,993, duration=2)

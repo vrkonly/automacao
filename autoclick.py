cec3b15487a8680b977dba4c107777da291c441d
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-08 11:13:11 -0300

    Rolar Scroll e clicar em link - pyautogui

--- autoclick.py
+++ autoclick.py
@@ -1,8 +1,7 @@
-# Criar uma pasta na Área de Trabalho
-
 import pyautogui
 
-pyautogui.click(x=740, y=113, clicks = 1, interval=0.0, button = 'right', duration=0)
-pyautogui.moveTo(781,388, duration=0)
-pyautogui.moveTo(1169,391, duration =0)
+pyautogui.moveTo(1911,96, duration=1)
+pyautogui.click()
+pyautogui.scroll(-1500)
+pyautogui.moveTo(565,477, duration=1)
 pyautogui.click()

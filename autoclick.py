30a035797a9c10aeffe445fbe4dc6ddf8af45176
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-09 11:59:27 -0300

    Att1

--- autoclick.py
+++ autoclick.py
@@ -1,6 +1,6 @@
 #pyautogui.alert(text='Estamos entrando em nossa Automação', title='Aviso')
 #email = pyautogui.prompt(text='Digite seu e-mail', title='Informações Obrigatórias')
-#print(f'você digitou {email}')
+#print(f'você digitou {email}'),
 #pyautogui.confirm(text=f'Continuar rodando nossa automação com o email {email}?', title='Confirmação', buttons=['Sim', 'Não', 'Cancelar'])
 #pyautogui.password(text='Digite sua senha:', title='Senha de Login', mask='*')
 

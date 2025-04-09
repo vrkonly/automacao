cc406214731c464f95c160dca40ed96d3302c95d
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-09 11:57:59 -0300

    att

--- autoclick.py
+++ autoclick.py
@@ -24,17 +24,17 @@ while operacao == True:
     if email == 'automacao@gmail.com' and senha == 'Testes@2':
         print('Parabéns, você acessou sua conta e agora vamos imprimir seus dados criando um bloco de notas.')
         pyautogui.click(x=813,y=26, duration=1.5, button='right')
-        pyautogui.click(858,304, duration=1.5)
-        pyautogui.moveTo(1244,307, duration=1.5)
-        pyautogui.click(1240,532, duration=1.5)
+        pyautogui.click(863,305, duration=1.5)
+        pyautogui.moveTo(1311,310, duration=1.5)
+        pyautogui.click(1308,530, duration=1.5)
         escrever('Automação')
         for x in range (1, 3):        
-            pyautogui.click(871,36)
+            pyautogui.click(870,39)
         escrever(f'E-mail: {email}\nSenha: {senha}')
         confirma = pyautogui.confirm(f'Você deseja continuar com a conta {email} e salvar estas informações neste bloco de notas?', title='Confirmação', buttons=['Sim', 'Não'])
         if confirma == 'Sim':
-            pyautogui.click(19,82, duration=1.5)
-            pyautogui.click(46,169, duration=1.5)
+            pyautogui.click(24,83, duration=1.5)
+            pyautogui.click(53,168, duration=1.5)
             operacao = False
     elif email != 'automacao@gmail.com':
         pyautogui.alert(text='Infelizmente você digitou um e-mail que não corresponde ao cadastrado neste sistema, favor tentar novamente.', title='Atenção!')

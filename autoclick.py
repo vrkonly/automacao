1fd5dbbc0215865426510708dc5115f70adf802a
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-11 10:56:20 -0300

    PROJETO 1

--- autoclick.py
+++ autoclick.py
@@ -3,20 +3,45 @@ import webbrowser
 import pyperclip
 import time
 
-
 def escrever(frase):
     pyperclip.copy(frase)
     pyautogui.hotkey('ctrl','v')
 
-webbrowser.open('https://cursoautomacao.netlify.app/?_gl=1*1edetzk*_ga*MTM4NTczNTY2NS4xNzQzMTcwMzA0*_ga_37GXT4VGQK*MTc0NDI5ODg3MC4yOS4xLjE3NDQyOTk2OTMuMC4wLjA.')
-pyautogui.click(1912,183, duration=2)
-pyautogui.scroll(-500)
-pyautogui.click(pyautogui.locateOnScreen('captura.PNG'), duration=2)
-escrever('Vinícius Romeu Kegles')
-pyautogui.click(1472,733, duration=2)
-pyautogui.click(1126,207, duration=2)
-pyautogui.scroll(1000)
-time.sleep(1.5)
-pyautogui.scroll(-2000)
-pyautogui.click(pyautogui.locateCenterOnScreen('excel.PNG'), duration=2)
-pyautogui.click(pyautogui.locateCenterOnScreen('pdf.PNG'), duration=2)
+for x in range(30):
+    webbrowser.open('https://www.instagram.com/')
+    pyautogui.click(1139,361, duration=2)
+    time.sleep(2)
+    escrever('pizzasoffice')
+    pyautogui.press('tab')
+    escrever('Paralelepipedada@2')
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.click(69,267, duration=2)
+    time.sleep(2)
+    escrever('Nike')
+    time.sleep(2)
+    pyautogui.click(171,256, duration=2)
+    time.sleep(2)
+    pyautogui.click(784,869, duration=2)
+    time.sleep(3)
+
+    try:
+        pyautogui.locateOnScreen('curtido.PNG')
+        print('Você já curtiu o post do dia de hoje.')
+    except pyautogui.ImageNotFoundException:
+        pyautogui.click(985,882, duration=2)
+
+    time.sleep(2)
+
+    try:
+        pyautogui.locateOnScreen('comentado.PNG')
+        print('Já existe um comentário no post de hoje.')
+    except pyautogui.ImageNotFoundException:
+        pyautogui.click(1073,989, duration=2)
+        escrever('Muito Top!')
+        pyautogui.press('enter')
+    time.sleep(86400)
+
+
+
+

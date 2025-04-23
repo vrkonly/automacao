da8c0a33303cfdcad97fca8ccb11c2b4c21e32d3
vrkonly <viniciuskegles.sst@gmail.com>
2025-04-23 11:28:32 -0300

    Bot de Mensagens no WhatsApp

--- py.py
+++ py.py
@@ -1,3 +1,30 @@
 import pyautogui
+import time
+import keyboard
+import pyperclip
 
-pyautogui.locateOnScreen('comentado.PNG')
+def escrever(frase):
+    pyperclip.copy(frase)
+    pyautogui.hotkey('ctrl', 'v')
+
+lista = ['Pai', 'Mãe']
+
+for x in lista:
+    time.sleep(1.5)
+    pyautogui.click(383,180, duration=2)
+    time.sleep(1.5)
+    pyautogui.hotkey('ctrl','a')
+    time.sleep(1)
+    pyautogui.press('delete')
+    time.sleep(1.5)
+    pyautogui.write(f'{x}')
+    time.sleep(1.5)
+    pyautogui.click(458,332, duration=2)
+    time.sleep(1.5)
+    pyautogui.click(873,993, duration=2)
+    time.sleep(1.5)
+    escrever('Teste')
+    pyautogui.press('enter')
+
+
+
